b04f80de2d3977b62a8e98cc065c79c7daa9d77e
ayesparshh <sparshgupta1001@gmail.com>
2023-12-31 23:17:00 +0530

    commit it

--- first.js
+++ first.js
@@ -1,6 +1,6 @@
-const { v4: uuidv4 } = require("uuid"); //when type is not set to module in package.json
+// const { v4: uuidv4 } = require("uuid"); //when type is not set to module in package.json
 // import { v4 as uuidv4 } from "uuid"; when in package.json type is module
-console.log(uuidv4());
+// console.log(uuidv4());
 
 // const http = require("http");
 // const myServer = http.createServer((req, res) => {
@@ -14,3 +14,36 @@ console.log(uuidv4());
 // myServer.listen(3000, () => {
 // 	console.log("jabtak samjh nhi aata tabtak try karte raho");
 // });
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = 3000;
+
+const customers = [
+	{
+		id: 1,
+		namee: "shubham",
+		mail: "subhna",
+	},
+	{
+		id: 2,
+		namee: "shubham",
+		mail: "subhna",
+	},
+];
+app.get("/", (req, res) => {
+	res.send({ customers: customers });
+});
+
+app.post("/", (req, res) => {
+	console.log(req.body);
+	res.send(req.body);
+});
+
+app.listen(PORT, () => {
+	console.log("server chla " + PORT);
+});
